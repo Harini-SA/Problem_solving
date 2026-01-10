422052f970ae914f006114a7a0688030c6a11f19
Harini-SA <harinisa1109@gmail.com>
2026-01-11 01:09:34 +0530

    check the arrays are same

--- arrays/check_equal_array.py
+++ arrays/check_equal_array.py
@@ -5,10 +5,12 @@ arr1 = array('i',[3,2,1])
 if len(arr)!=len(arr1):
     print("not same")
 else:
-   freq = Counter(arr)
-   for i in arr1:
-       if i not in freq:
+    freq = Counter(arr)
+    freq1 = Counter(arr1)
+    for key in freq.keys():
+        if freq[key] != freq1[key]:
            print("not equal")
-   else:
+           break
+    else:
        print("same")
                
