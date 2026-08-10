fd5ce5073e8120ffef8f8fa28deb047595389db2
Harini-SA <harinisa1109@gmail.com>
2026-08-10 14:03:18 +0530

    to detect a loop

---
+++ linked_list/detect_a_loop.py
@@ -0,0 +1,10 @@
+def detectLoop(self, head):
+        # code here
+        slow  = head
+        fast = head
+        while slow and fast and fast.next :
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False 
