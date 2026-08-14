bec429f34512b8622c14ab8fe9c2d75ea9862a75
Harini-SA <harinisa1109@gmail.com>
2026-08-14 21:02:37 +0530

    to reverse a double linked list

---
+++ linked_list/reverse_dll.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None        
+        self.prev = None
+n1 = Node(1)
+n2 = Node(3)
+n2.prev = n1
+n1.next = n2
+n3 = Node(2)
+n2.next = n3
+n3.prev= n2
+def reverse(head):
+    temp = None
+    curr = head
+    while curr is not None:
+        temp = curr.prev
+        curr.prev = curr.next
+        curr.next = temp
+        curr = curr.prev
+    if temp is not None:
+        head = temp.prev 
+    return head       
+
+    
+    return head           
+def print_dll(head):
+    curr = head
+    while curr:
+        print(curr.data, end =" ")
+        if curr.next is not None:
+            print("<->", end =" ")
+        curr = curr.next                
+print_dll(reverse(n1))                
