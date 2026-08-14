9b7f6cc81ff896e7107e63851384d559b7155466
Harini-SA <harinisa1109@gmail.com>
2026-08-14 21:04:56 +0530

    to insert element at end

---
+++ linked_list/insert_end_dll.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None        
+        self.prev = None
+n1 = Node(1)
+n2 = Node(3)
+n2.prev = n1
+n1.next = n2
+n3 = Node(2)
+n2.next = n3
+n3.prev= n2
+def insert(data,head):
+    curr = head
+    while curr.next is not None:
+        curr = curr.next
+    newnode = Node(data)
+    curr.next = newnode
+    newnode.prev = curr    
+    return head           
+def print_dll(head):
+    curr = head
+    while curr:
+        print(curr.data, end =" ")
+        if curr.next is not None:
+            print("<->", end =" ")
+        curr = curr.next                
+print_dll(insert(4,n1))                
